72b69a71961a22e2727e0da5338b7eae7dc09455
psycon <daniel.hoff@1und1.de>
2014-05-20 15:28:12 +0200

    Added init.d script

--- kippo_installation.sh
+++ kippo_installation.sh
@@ -4,7 +4,7 @@ KIPPO_INSTALL_DIR="/opt/kippo/"
 APT_CMD=$(which apt-get)
 APT_OPTS="--yes --no-install-recommends"
 
-$APT_CMD $APT_OPTS install python-mysqldb iptables-persistent pwgen
+$APT_CMD $APT_OPTS install python-twisted python-mysqldb iptables-persistent pwgen
 
 # download kippo
 cd /tmp/
@@ -41,6 +41,67 @@ username = kippo
 password = ${kippo_pw}
 port = 3306
 EOL
+
+adduser --system --home ${KIPPO_INSTALL_DIR} --disabled-login kippo
+chown -R kippo:nogroup ${KIPPO_INSTALL_DIR}
+
+cat > /etc/init.d/kippo <<EOF
+#!/bin/bash
+
+### BEGIN INIT INFO
+# Provides:          kippo
+# Required-Start:    \$remote_fs \$network \$syslog
+# Required-Stop:     \$remote_fs \$syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start kippo
+# Description:       Kippo is a SSH honeypot.
+### END INIT INFO
+
+NAME="kippo"
+DESC="Kippo Honeypot"
+PIDDIR="/var/run/\$NAME"
+PIDFILE="\$PIDDIR/\$NAME.pid"
+SCRIPTNAME="/etc/init.d/\$NAME"
+
+DAEMON_PATH="${KIPPO_INSTALL_DIR}"
+DAEMON="${which twistd}"
+DAEMON_ARGS="-y kippo.tac -l log/kippo.log --pidfile \$PIDFILE"
+ 
+[ -d "\$PIDDIR" ] || mkdir -p "\$PIDDIR" && chown kippo "\$PIDDIR"
+
+
+case "\$1" in
+
+start) 
+        echo -n "Starting \$DESC: "
+        start-stop-daemon --start --chdir \$DAEMON_PATH --chuid kippo --background --pidfile \$PIDFILE --exec \$DAEMON -- \$DAEMON_ARGS && echo "OK"
+        ;;
+
+stop)
+        echo -n "Stopping \$DESC: "
+        start-stop-daemon --stop --pidfile \$PIDFILE && echo "OK"
+        ;;
+
+restart)
+        echo "Restarting \$DESC: " 
+        $0 stop
+        sleep 1
+        $0 start
+        ;;
+
+*)
+        echo "Usage: \$0 {start|stop|restart}"
+        exit 1
+        ;;
+
+esac
+
+exit 0
+EOF
+
+chmod +x /etc/init.d/glastopf
+update-rc.d glastopf defaults
 
 
 # move ssh port away from port 22
